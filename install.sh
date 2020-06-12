1259ce3a09cdd846c14e1e392cc7e048ec3b2fe2
premroxx <premroxx@gmail.com>
2020-06-12 17:26:21 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Chef Client Install
+cd /tmp/
+wget https://www.opscode.com/chef/install.sh | sudo bash
+chef-client -v
+
+#Run Cookbook
+# mkdir /var/chef/
+# mkdir /var/chef/cookbooks
+# cd /var/chef/cookbooks
+# chown -R cfadmin /var/chef/cookbooks/
+# knife supermarket download sudo
+# tar -zxvf *.tar.gz
+
+#Upload Cfusion bin
+#tar -zxvf *.tar.gz
