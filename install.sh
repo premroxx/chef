77dc9117daa754a84c1334eeffcae6e08cbbb2fb
premroxx <premroxx@gmail.com>
2020-06-14 23:35:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Chef Client Install
-echo "$USER" > test.txt
+echo "$USER" > test1.txt
 cd /tmp/
 #wget https://www.opscode.com/chef/install.sh | sudo bash
 #curl -L https://omnitruck.chef.io/install.sh | sudo bash
